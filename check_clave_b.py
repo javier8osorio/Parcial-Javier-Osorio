3666a7a9ff65f15446fcf612fe9ca0a4ed269214
javier8osorio <71994352+javier8osorio@users.noreply.github.com>
2020-10-15 21:02:41 -0600

    Add files via upload

---
+++ check_clave_b.py
@@ -0,0 +1,83 @@
+from clave_b import (
+    suma,
+    sumaImpares,
+    definicionEsfera,
+    Esfera,
+    Banco,
+    Cliente,
+    getGithubUrl,
+)
+
+print("Clave B...")
+
+
+# ejercicio 1 -->
+result = suma(2, 4, 6)
+if result == 12:
+    print("ejercicio01: pass")
+else:
+    print("ejercicio01: fail")
+
+
+# ejercicio 2 -->
+result = sumaImpares()
+if result == 250000:
+    print("ejercicio02: pass")
+else:
+    print("ejercicio02: fail")
+
+
+# ejercicio 3 -->
+result = definicionEsfera(12)
+if result == {
+    "perimetro": 75.39822368615503,
+    "area": 1809.5573684677208,
+    "volumen": 7238.229473870882,
+}:
+    print("ejercicio03: pass")
+else:
+    print("ejercicio03: fail")
+
+
+# ejercicio 4 -->
+esfera = Esfera()
+result = esfera.definicionEsfera(12)
+if result == {
+    "perimetro": 75.39822368615503,
+    "area": 1809.5573684677208,
+    "volumen": 7238.229473870882,
+}:
+    print("ejercicio04: pass")
+else:
+    print("ejercicio04: fail")
+
+# ejercicio 5 -->
+banco = Banco()
+banco.procesar("balbino", "san salvador", 1, "abono", 250.0)
+banco.procesar("rodrigo", "san salvador", 2, "retiro", 350.0)
+banco.procesar("marta", "san salvador", 3, "abono", 400.0)
+banco.procesar("rafael", "santa ana", 4, "abono", 500.0)
+banco.procesar("balbino", "la libertad", 1, "abono", 250.0)
+banco.procesar("marta", "san salvador", 3, "abono", 250.0)
+banco.procesar("rodrigo", "san salvador", 2, "abono", 100.0)
+# total de abonos en san salvador
+totalAbonos = banco.abonosSanSalvador()
+if totalAbonos == 1000.0:
+    print("ejercicio05part01: pass")
+else:
+    print("ejercicio05part01: fail")
+
+# total de abonos entre balbino y rodrigo
+totalAbonos = banco.abonosBalYRod()
+if totalAbonos == 600.0:
+    print("ejercicio05part02: pass")
+else:
+    print("ejercicio05part02: fail")
+
+
+# ejercicio 6 -->
+result = getGithubUrl()
+if not result == "":
+    print("ejercicio06: pass")
+else:
+    print("ejercicio06: fail")
---
+++ clave_b.py
@@ -0,0 +1,191 @@
+from math import pi
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 3 numeros
+2+4+6 = 12
+"""
+
+
+# start-->
+def suma(numero1, numero2, numero3):
+    resultado = numero1 + numero2 + numero3
+    print(resultado)
+    return resultado
+
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+la suma de los numeros impares del 1 al 1000
+"""
+
+
+# start-->
+def sumaImpares():
+    numeros = []
+
+    numero = 0
+    while numero<1000:
+        numero+=1
+        if numero % 2 != 0:
+            numeros.append(numero) #Añade el numero al array
+
+
+    result = sum(numeros)
+
+    print(result)
+
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+encontrar el perimetro, area y el volumen de un esfera
+radio = 12 m
+
+perimetro: 2*pi*r
+area: 4*pi*r^2
+volumen: (4/3)*pi*r^3
+"""
+#Profe en este no me da el pass, asi que le imprimi 
+#los valores ya que los valores si me dan
+
+# start-->
+def definicionEsfera(radio):
+    result = print(f"perimetro: {obtenerPerimetro()} area: {obtenerArea()} volumen: {obtenerVolumen()}")
+    return result
+
+
+def obtenerPerimetro():
+    result = float(2 * pi * 12)
+    return result
+
+
+def obtenerArea():
+    result = float(4 * pi * 12**2)
+    return result
+
+
+def obtenerVolumen():
+    result = float((4/3) * pi * 12**3)
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+el ejercicio numero 3 convertirlo en una clase
+
+"""
+
+#Profe en este no me da el pass, asi que le imprimi 
+#los valores ya que los valores si me dan
+
+
+# start-->
+class Esfera:
+    def definicionEsfera(self, radio):
+        self.radio = radio
+        result = print(f"perimetro: {obtenerPerimetro()} area: {obtenerArea()} volumen: {obtenerVolumen()}")
+        return result
+
+    def obtenerPerimetro(self):
+        result = float(2 * pi * self.radio)
+        return result
+
+
+    def obtenerArea(self):
+        result = float(4 * pi * self.radio**2)
+        return result
+
+
+    def obtenerVolumen(self):
+        result = float((4/3) * pi * self.radio**3)
+        return result
+        
+    
+
+
+"""
+***************************************************************
+@@ ejercicio 5 @@
+Banco
+Cliente
+    nombre
+    lugar
+    numero de cuenta
+    transaccion - retiro o abono
+    monto
+"""
+
+
+class Banco:
+    def __init__(self):
+        self.listaClientes = []
+
+    def procesar(self, nombre, ciudad, id, operacion, monto):
+        cliente = Cliente(nombre, ciudad, id, operacion, monto)
+        self.listaClientes.append(cliente)
+
+    def abonosSanSalvador(self):
+        abonototal = 0
+        for Cliente in self.listaClientes:
+            ciudad = Cliente.getCiudad()
+            proceso = Cliente.getOperacion()
+            if ciudad == "san salvador" and proceso == "abono":
+                abonototal = abonototal + Cliente.getMonto()
+        return abonototal
+
+
+    def abonosBalYRod(self):
+        abonototal = 0
+        for Cliente in self.listaClientes:
+            nombre1 = Cliente.getNombre()
+            proceso = Cliente.getOperacion()
+            nombre2 = Cliente.getNombre()
+            if (nombre1 == "rodrigo" or nombre2 == "balbino") and proceso == "abono"  :
+                abonototal = abonototal + Cliente.getMonto()
+        return abonototal
+
+
+class Cliente:
+    def __init__ (self, nombre, ciudad, id, operacion, monto):
+        self.nombre = nombre
+        self.ciudad = ciudad
+        self.id = id
+        self.operacion = operacion
+        self.monto = monto
+
+    def getNombre(self):
+        return self.nombre
+
+    def getCiudad(self):
+        return self.ciudad
+
+    def getId(self):
+        return self.id
+
+    def getOperacion(self):
+        return self.operacion
+
+    def getMonto(self):
+        return self.monto
+
+
+"""
+***************************************************************
+@@ ejercicio 6 @@
+colocar este proyecto en github
+colocar aca debajo la url
+ademas colocar la url en un archivo
+github_<nombre>_<codigo>.txt y subirlo a moodle
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return ""
